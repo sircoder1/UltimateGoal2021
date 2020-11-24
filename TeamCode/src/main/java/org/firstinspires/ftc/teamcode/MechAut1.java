f39379f450b6f069192f823e2c50db4045e43d9c
joshfaber <55210510+joshfaber@users.noreply.github.com>
2020-11-24 17:12:43 -0500

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechAut1.java
@@ -0,0 +1,45 @@
+/*
+First attempt at autonomous code Destriers 2021
+Created by Joel Samuel with Josh Faber and Jamie Heskett
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="MechAut1", group="Auton")
+
+public class MechAut1 extends LinearOpMode {
+
+    private DcMotor frontLeftMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor backRightMotor;
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    RobotGeneral Robot = new RobotGeneral(frontRightMotor, frontLeftMotor, backRightMotor, backLeftMotor, telemetry, this);
+
+    @Override
+
+    public void runOpMode() {
+        Robot.init(hardwareMap);
+
+        telemetry.addData("Status", "Auton has been Initialized");
+        telemetry.update();
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive() && runtime.seconds()<2) {
+
+            Robot.move(0,0.5);
+        }
+
+        Robot.move(0,0);
+
+    }
+}
